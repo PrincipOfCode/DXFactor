ab43fa7cfffa9b726bd6ddd0a4afd23fd1b47970
PrincipOfCode <iviy.smtp@gmail.com>
2025-04-17 02:57:36 +0300

    #GUI updates

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,5 +20,21 @@ namespace DXFactor
         {
             InitializeComponent();
         }
+
+        private void ToolBar_Loaded(object sender, RoutedEventArgs e)
+        {
+            ToolBar toolBar = sender as ToolBar;
+            var overflowGrid = toolBar.Template.FindName("OverflowGrid", toolBar) as FrameworkElement;
+            if (overflowGrid != null)
+            {
+                overflowGrid.Visibility = Visibility.Collapsed;
+            }
+
+            var mainPanelBorder = toolBar.Template.FindName("MainPanelBorder", toolBar) as FrameworkElement;
+            if (mainPanelBorder != null)
+            {
+                mainPanelBorder.Margin = new Thickness(0);
+            }
+        }
     }
 }
